0954fe56c8019acd5c1a889526660c204c6c3648
SansGHG <User@DESKTOP-PB8H09E.sunrise.box>
2022-02-24 18:10:14 +0100

    added a command

---
+++ src/main/java/net/mcreator/thetriopack/procedures/TheMemeCommandExecutedProcedure.java
@@ -0,0 +1,39 @@
+package net.mcreator.thetriopack.procedures;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.BlockPos;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
+
+import java.util.Iterator;
+
+public class TheMemeCommandExecutedProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if (entity instanceof LivingEntity _entity) {
+			ItemStack _setstack = new ItemStack(Blocks.LAVA);
+			_setstack.setCount(1);
+			_entity.setItemInHand(InteractionHand.MAIN_HAND, _setstack);
+			if (_entity instanceof ServerPlayer _serverPlayer)
+				_serverPlayer.getInventory().setChanged();
+		}
+		world.setBlock(new BlockPos((int) x, (int) y, (int) z), Blocks.WAXED_COPPER_BLOCK.defaultBlockState(), 3);
+		if (entity instanceof ServerPlayer _player) {
+			Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("the_trio_pack:thewow"));
+			AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+			if (!_ap.isDone()) {
+				Iterator _iterator = _ap.getRemainingCriteria().iterator();
+				while (_iterator.hasNext())
+					_player.getAdvancements().award(_adv, (String) _iterator.next());
+			}
+		}
+	}
+}
